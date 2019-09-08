7cd4315dff1c80f6a98dd4bd0ef4d43a231e652e
zhou <282837349@qq.com>
2019-09-08 16:57:07 +0800

    创建

--- gmall0401-user-manage/src/main/java/com/atguigu/gmall0401/gmall0401user/Gmall0401UserManageApplication.java
+++ gmall0401-user-manage/src/main/java/com/atguigu/gmall0401/gmall0401user/Gmall0401UserManageApplication.java
@@ -3,6 +3,9 @@ package com.atguigu.gmall0401.gmall0401user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import tk.mybatis.spring.annotation.MapperScan;
+
+@MapperScan(basePackages = "com.atguigu.gmall0401.gmall0401user.mapper")
 @SpringBootApplication
 public class Gmall0401UserManageApplication {
 
